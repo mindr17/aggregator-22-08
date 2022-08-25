d285e3142d456fc185331d847660efba45ce3674
mindr17 <mindr17@yandex.ru>
2022-08-25 10:49:02 +0300

    fix: add all files

---
+++ frontend/pages/index-test.tsx
@@ -0,0 +1,42 @@
+
+import { useEffect, useRef, useState } from 'react';
+import { socketInterface } from '../src/modules/interfaces';
+import { MySocket } from '../src/modules/MySocket';
+
+function Home (props: any) {
+  const [messages, setMessages] = useState(['Template message']);
+
+  useEffect(() => {
+    const chatSocket: socketInterface = new MySocket();
+
+    const handleNewsEvent = (event: any) => {
+      setMessages(lastState => {
+        return [event.detail.msg, ...lastState];
+      });
+    };
+
+    window.addEventListener('news', handleNewsEvent);
+
+    return (
+      () => {
+        chatSocket.destroy();
+
+        window.removeEventListener('news', handleNewsEvent);
+      }
+    );
+  }, []);
+
+  return (
+    <>
+      <div>
+        {
+          messages.map(message => {
+            return <div key={message}>{message}</div>;
+          })
+        }
+      </div>
+    </>
+  );
+};
+
+export default Home;
