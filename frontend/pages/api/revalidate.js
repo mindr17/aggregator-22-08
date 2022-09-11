3341b9351705b07490031cd856eaf70690615336
mindr17 <mindr17@yandex.ru>
2022-09-11 14:19:15 +0300

    feat: vercel

--- frontend/pages/api/revalidate.js
+++ frontend/pages/api/revalidate.js
@@ -1,4 +1,4 @@
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
   // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
   if (req.query.secret !== 'KIGOz3d9H31FGDD3klts0qhRqfq') {
--- frontend/upload-banana.sh
+++ frontend/upload-banana.sh
@@ -2,13 +2,16 @@ HOST="banana"
 PROJECT_NAME="aggregator-frontend"
 BACKEND_DIR="/root/aggregator/frontend"
 
-echo "Building"
+echo "Building locally"
 npm run build
 
 echo "Uploading"
 rsync --files-from=rsync-files -r --delete . $HOST:$BACKEND_DIR || exit 2
 
+echo "Building remotely"
+# ssh $HOST:$BACKEND_DIR npm run build || exit 4
+
 echo "Reloading process"
-ssh $HOST pm2 reload $PROJECT_NAME || exit 4
+# ssh $HOST pm2 reload $PROJECT_NAME || exit 4
 
 echo "DONE"
