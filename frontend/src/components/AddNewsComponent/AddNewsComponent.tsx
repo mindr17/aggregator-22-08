259bc43143e0cd41ef2c0414e18a3b29a7f1d7c8
mindr17 <mindr17@yandex.ru>
2022-09-15 00:02:33 +0300

    feat: remember to change handlers for form

--- frontend/src/components/AddNewsComponent/AddNewsComponent.tsx
+++ frontend/src/components/AddNewsComponent/AddNewsComponent.tsx
@@ -2,32 +2,71 @@ import { useState } from 'react';
 import styles from './AddNewsComponent.module.scss';
 
 const AddNewsComponent = (props: any) => {
-  const [burgerState, setBurgerState] = useState<boolean>(false);
-
-  const [searchState, setSearchState] = useState('');
+  const [timeState, setTimeState] = useState('');
+  const [titleState, setTitleState] = useState('');
+  const [textState, setTextState] = useState('');
+  const [form, setForm] = useState({
+    
+  });
 
   const handleSearchChange = (e: { target: { value: any; }; }) => {
-    setSearchState(e.target.value);
+    setTimeState(e.target.value);
   };
 
   const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
+    
+    const article = {
+      time: timeState,
+      title: titleState,
+      text: textState,
+    }
 
-    props.update({
-      search: searchState,
-    });
+    console.log(article);
+
+    // props.update({
+    //   article,
+    // });
+  };
+
+  const changeHandler = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   return (
     <div className={styles.addNews}>
       <form className={styles.addNewsForm} onSubmit={handleSubmit}>
-        <textarea
-          className={styles.textArea}
-          placeholder="Search in news titles..."
-          onChange={handleSearchChange}
-          value={searchState}
-        >
-        </textarea>
+        <div className={styles.block}>
+          Time
+          <textarea
+            className={styles.textArea}
+            placeholder="Input news time"
+            onChange={handleSearchChange}
+            value={timeState}
+          >
+          </textarea>
+        </div>
+        <div className={styles.block}>
+          Title
+          <textarea
+            className={styles.textArea}
+            placeholder="Input news title"
+            onChange={handleSearchChange}
+            value={titleState}
+          >
+          </textarea>
+        </div>
+        <div className={styles.block}>
+          Text
+          <textarea
+            className={styles.textArea}
+            placeholder="Input news text"
+            onChange={handleSearchChange}
+            value={textState}
+          >
+          </textarea>
+        </div>
+        <button className={styles.addArticleBtn}>Add article</button>
       </form>
     </div>
   );
